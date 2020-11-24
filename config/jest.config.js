e028136ed533f803cafd32187cceadaa58c7ff2e
Jakub Miskech <yakub@Jakubs-MacBook-Pro.local>
2020-11-24 15:00:07 +0100

    fix cci and codecov

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     rootDir: '../',
     preset: 'ts-jest',
+    collectCoverage: true,
     collectCoverageFrom: ['<rootDir>/src/**/*.{ts,js}'],
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/test/__mocks__/jest.setup.js'],
