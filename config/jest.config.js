257d8ad3f7a5af99ca19df49b4307034e7cd4ebe
Jakub Miskech <yakub@Jakubs-MacBook-Pro.local>
2020-11-24 15:57:04 +0100

    add junit for test results

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
     rootDir: '../',
     preset: 'ts-jest',
+    reporters: ['default', 'jest-junit'],
     collectCoverage: true,
     collectCoverageFrom: ['<rootDir>/src/**/*.{ts,js}'],
     testEnvironment: 'jsdom',
+    testResultsProcessor: 'jest-junit',
     setupFilesAfterEnv: ['<rootDir>/test/__mocks__/jest.setup.js'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/__mocks__/fileMock.js',
         '\\.(css|less)$': '<rootDir>/test/__mocks__/styleMock.js',
     },
+    reporters: ['default', ['jest-junit', { outputDirectory: './coverage' }]],
 };
